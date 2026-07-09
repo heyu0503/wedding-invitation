f652b71c3e9eed5c136413d080b302cedd452300
heyu0503 <heejung9905@gmail.com>
2026-07-09 12:41:41 +0900

    문구 간격 수정

--- src/component/information/index.tsx
+++ src/component/information/index.tsx
@@ -16,7 +16,7 @@ export const Information1 = () => {
       <div className="info-card">
         <div className="label">식사 안내</div>
         <div className="content">
-          식사시간: 14시 20분 ~ 16시 40분
+          식사시간: 14시 20분 - 16시 40분
           <br />
           장소: 4-5층 연회장
         </div>
